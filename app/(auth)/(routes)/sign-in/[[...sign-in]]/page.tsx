b656ab431c71469a7f0089ed0cc7d4ae191131c2
Atik Shahriar Ovi <atikshahriar@iut-dhaka.edu>
2024-11-10 15:31:44 +0600

    Fix sign-in page

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,15 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <>
+      <div>
+        <SignIn />
+        {/* <div className="text-sm text-slate-500 mr-auto pt-2">
+          <Link href="/forgot-password/">   Forgot     Password?</Link>
+        </div> */}
+      </div>
+    </>
+  );
 }
